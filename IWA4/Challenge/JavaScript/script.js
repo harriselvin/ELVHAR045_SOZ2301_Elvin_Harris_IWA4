9de807b54578068bdcfa0b056b3647c18236fa41
Elvin <115928113+harriselvin@users.noreply.github.com>
2023-04-03 11:46:24 +0200

    Updated script.js

--- IWA4/Challenge/JavaScript/script.js
+++ IWA4/Challenge/JavaScript/script.js
@@ -1,5 +1,5 @@
 let date = 2050
-let status = 'student'
+let status = 'parent'
 let count = 0
 
 if (date = 2050) {
@@ -10,7 +10,7 @@ if (date = 2050) {
 	console.log(date, 'Freedom Day')
 	count = count + 4;
 
-	if (status == "student") {
+	if (status === "student") {
 	  console.log('June', 'Youth Day')
 	  count = count + 1;
   }
@@ -21,8 +21,9 @@ if (date = 2050) {
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3;
 
-	if (status = "parent") {
+	if (status === "parent") {
 	  console.log(date, 'Christmas Day')
+	  count = count + 1;
   }
 
 	console.log(date, 'Day of Goodwill')
